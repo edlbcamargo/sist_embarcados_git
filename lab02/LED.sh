fe1c27b424af1f2d8f05a1ee48535e506304603f
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2018-10-07 09:54:49 -0300

    Adicionando script bash para acender led.

---
+++ lab02/LED.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+LED_GPIO=16  # Use a variable -- easy to change GPIO number
+
+# An example Bash functions
+function setLED
+{                                      # $1 is the 1st argument passed to this function
+	echo $1 >> "/sys/class/gpio/gpio$LED_GPIO/value"
+}
+
+# Start of the program -- start reading from here
+if [ $# -ne 1 ]; then                  # if there is not exactly one argument
+	echo "No command was passed. Usage is: bashLED command,"
+	echo "where command is one of: setup, on, off, status and close"
+	echo -e " e.g., bashLED setup, followed by bashLED on"
+	exit 2                         # error that indicates invalid number of arguments
+fi
+
+echo "The LED command that was passed is: $1"
+
+if [ "$1" == "setup" ]; then
+	echo "Exporting GPIO number $LED_GPIO"
+	echo $LED_GPIO >> "/sys/class/gpio/export"
+	sleep 1                        # to ensure gpio has been exported before next step
+	echo "out" >> "/sys/class/gpio/gpio$LED_GPIO/direction"
+elif [ "$1" == "on" ]; then
+	echo "Turning the LED on"
+	setLED 1                       # 1 is received as $1 in the setLED function
+elif [ "$1" == "off" ]; then
+	echo "Turning the LED off"
+	setLED 0                       # 0 is received as $1 in the setLED function
+elif [ "$1" == "status" ]; then
+	state=$(cat "/sys/class/gpio/gpio$LED_GPIO/value")
+	echo "The LED state is: $state"
+elif [ "$1" == "close" ]; then
+	echo "Unexporting GPIO number $LED_GPIO"
+	echo $LED_GPIO >> "/sys/class/gpio/unexport"
+else
+	echo "Unrecongnized command."
+	exit 3                         # error that indicates unrecognized command
+fi
