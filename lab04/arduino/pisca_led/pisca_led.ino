76832bfd0013d1aaff6e0a16693762ab52b5c6bf
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2018-10-20 13:14:48 -0300

    Adicionando informacao do pino utilizado como saida.

--- lab04/arduino/pisca_led/pisca_led.ino
+++ lab04/arduino/pisca_led/pisca_led.ino
@@ -1,9 +1,9 @@
 // a funcao setup roda apenas uma vez, quando a placa eh alimentada
 // ou o botao reset eh pressionado
 void setup() {
-  Serial.begin(9600, SERIAL_8N1);  // configura a comunicacao serial
-  pinMode(LED_BUILTIN, OUTPUT);     // inicializa o pino digital LED_BUILTIN como saida
-  Serial.write("Final da configuracao\n"); // envia uma mensagem pela comunicacao serial
+  Serial.begin(9600, SERIAL_8N1);   // configura a comunicacao serial
+  pinMode(LED_BUILTIN, OUTPUT);     // inicializa o pino digital LED_BUILTIN (pino 13) como saida
+  Serial.write("Final da configuracao. Saida no pino %d.\n",LED_BUILTIN); // envia uma mensagem pela comunicacao serial
 }
 
 // a funcao loop roda sem parar...
