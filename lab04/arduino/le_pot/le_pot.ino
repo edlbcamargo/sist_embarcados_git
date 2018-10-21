47fcae96c03d8718d1a3ecda5fc9e19709793680
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2018-10-21 11:08:53 -0300

    Atualizando frequencia de aquisicao.

--- lab04/arduino/le_pot/le_pot.ino
+++ lab04/arduino/le_pot/le_pot.ino
@@ -1,4 +1,4 @@
-const int analogInPin = A0;
+const int analogInPin = A0;                  // o potenciômetro esta ligado ao pino A0
 
 void setup(){
    // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
@@ -8,10 +8,10 @@ void setup(){
 void loop(){
    int valor = analogRead(analogInPin);      // le valor no pino A0 usando conversor ADC de 10-bit
    float potencial = valor * (5.0f/1023.0f); // converte o valor lido em potencial
-   Serial.print("Valor lido: ");
+   Serial.print("Valor lido: ");             // envia os valores pela porta serial
    Serial.print(valor);
    Serial.print("; Potencial correspondente: );
    Serial.print(potencial);
-   Serial.print("\n");
-   delay(500);
+   Serial.print("V.\n");
+   delay(1000);
 }
