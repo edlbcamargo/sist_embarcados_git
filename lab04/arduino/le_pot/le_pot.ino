bad7c0e3891ec9fda2c93ffdc5cf34eb5e615378
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2018-10-20 22:21:04 -0300

    Adicionando le_pot.ino.

---
+++ lab04/arduino/le_pot/le_pot.ino
@@ -0,0 +1,17 @@
+const int analogInPin = A0;
+
+void setup(){
+   // Configura a serial: baud rate de 115200, 8-bit, sem paridade, 1 stop bit
+   Serial.begin(115200, SERIAL_8N1);
+}
+
+void loop(){
+   int valor = analogRead(analogInPin);      // le valor no pino A0 usando conversor ADC de 10-bit
+   float potencial = valor * (5.0f/1023.0f); // converte o valor lido em potencial
+   Serial.print("Valor lido: ");
+   Serial.print(valor);
+   Serial.print("; Potencial correspondente: );
+   Serial.print(potencial);
+   Serial.print("\n");
+   delay(500);
+}
