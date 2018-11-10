ab94d2db7d10bb1d4d6d521fce055f3659dd43e1
edlbcamargo <43915520+edlbcamargo@users.noreply.github.com>
2018-11-10 12:08:05 -0200

    Incluindo arquivos do gnuplot.

---
+++ lab07/gnuplot/grafico.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ARQUIVODADOS=./dados.txt
+ARQUIVOSAIDA=./dados.png
+
+gnuplot << EOF
+set title "Título"
+set ylabel "Eixo Y"
+set xlabel "Eixo X"
+set terminal png
+set output "$ARQUIVOSAIDA"
+plot "$ARQUIVODADOS" linecolor rgb '#0060ad' linetype 1 linewidth 1 pointtype 2 pointsize 1.0 with linespoints
+EOF
+
